67e0f785c0b8facb66877cbd4ae0fde85a85830e
Fareed Rezaei <fareed9@msn.com>
2023-07-23 13:29:14 +0100

    change  type of date to date instead of calendar

--- src/qetaa/service/user/helpers/AppConstants.java
+++ src/qetaa/service/user/helpers/AppConstants.java
@@ -1,8 +1,8 @@
 package qetaa.service.user.helpers;
 
 public class AppConstants {
-	private final static String CUSTOMER_SERVICE = "http://localhost:8081/service-qetaa-customer/rest/";
-	private final static String VENDOR_SERVICE = "http://localhost:8081/service-qetaa-vendor/rest/";
+	private final static String CUSTOMER_SERVICE = "http://localhost:8080/service-qetaa-customer/rest/";
+	private final static String VENDOR_SERVICE = "http://localhost:8080/service-qetaa-vendor/rest/";
 	
 	public static final String CUSTOMER_MATCH_TOKEN = CUSTOMER_SERVICE + "match-token";
 	public static final String VENDOR_MATCH_TOKEN = VENDOR_SERVICE + "match-token";
